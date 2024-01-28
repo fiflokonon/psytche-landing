3f7b8953e518a6ea1d9d6de95e93c6e8d1388b8c
fiflokonon <fiflokonon@gmail.com>
2024-01-28 23:01:19 +0100

    New template

--- js/main.js
+++ js/main.js
@@ -4,46 +4,25 @@
     $(".carousel-inner .item:first-child").addClass("active");
     /* Mobile menu click then remove
     ==========================*/
-    $(".mainmenu-area #primary_menu li a").on("click", function () {
+    $(".mainmenu-area #mainmenu li a").on("click", function () {
         $(".navbar-collapse").removeClass("in");
+    });
+    /*WoW js Active
+    =================*/
+    new WOW().init({
+        mobile: true,
     });
     /* Scroll to top
     ===================*/
     $.scrollUp({
-        scrollText: '<i class="lnr lnr-arrow-up"></i>',
+        scrollText: '<i class="fa fa-angle-up"></i>',
         easingType: 'linear',
         scrollSpeed: 900,
         animation: 'fade'
     });
     /* testimonials Slider Active
     =============================*/
-    $('.gallery-slide').owlCarousel({
-        loop: true,
-        margin: 0,
-        responsiveClass: true,
-        nav: false,
-        autoplay: true,
-        autoplayTimeout: 4000,
-        smartSpeed: 1000,
-        navText: ['<i class="lnr lnr-chevron-left"></i>', '<i class="lnr lnr-chevron-right"></i>'],
-        responsive: {
-            0: {
-                items: 1,
-            },
-            600: {
-                items: 2
-            },
-            1280: {
-                items: 3
-            },
-            1500: {
-                items: 4
-            }
-        }
-    });
-    /* testimonials Slider Active
-    =============================*/
-    $('.team-slide').owlCarousel({
+    $('.testimonials').owlCarousel({
         loop: true,
         margin: 0,
         responsiveClass: true,
@@ -51,34 +30,131 @@
         autoplay: true,
         autoplayTimeout: 4000,
         smartSpeed: 1000,
-        navText: ['<i class="lnr lnr-chevron-left"></i>', '<i class="lnr lnr-chevron-right"></i>'],
+        navText: ['<i class="ti-arrow-left"></i>', '<i class="ti-arrow-right" ></i>'],
+        items: 1
+    });
+    /* testimonials Slider Active
+    =============================*/
+    $('.screen-slider').owlCarousel({
+        loop: true,
+        margin: 0,
+        responsiveClass: true,
+        nav: true,
+        autoplay: true,
+        autoplayTimeout: 4000,
+        smartSpeed: 1000,
+        navText: ['<i class="ti-arrow-left"></i>', '<i class="ti-arrow-right" ></i>'],
+        items: 1,
+        animateIn: 'fadeIn',
+        animateOut: 'fadeOut',
+        center: true,
+    });
+    /* testimonials Slider Active
+    =============================*/
+    $('.clients').owlCarousel({
+        loop: true,
+        margin: 30,
+        responsiveClass: true,
+        nav: true,
+        autoplay: true,
+        autoplayTimeout: 4000,
+        smartSpeed: 1000,
+        navText: ['<i class="ti-arrow-left"></i>', '<i class="ti-arrow-right" ></i>'],
         responsive: {
             0: {
-                items: 1,
+                items: 3,
             },
             600: {
-                items: 2
+                items: 4
             },
             1000: {
-                items: 3
+                items: 6
             }
         }
     });
-    $(".toggole-boxs").accordion();
-    /*---------------------------
-    MICHIMP INTEGRATION
-    -----------------------------*/
-    $('#mc-form').ajaxChimp({
-        url: 'https://quomodosoft.us14.list-manage.com/subscribe/post?u=b2a3f199e321346f8785d48fb&amp;id=d0323b0697', //Set Your Mailchamp URL
-        callback: function (resp) {
-            if (resp.result === 'success') {
-                $('.subscrie-form, .join-button').fadeOut();
-                $('body').css('overflow-y', 'scroll');
+    /*--------------------
+       MAGNIFIC POPUP JS
+       ----------------------*/
+    var magnifPopup = function () {
+        $('.work-popup').magnificPopup({
+            type: 'image',
+            removalDelay: 300,
+            mainClass: 'mfp-with-zoom',
+            gallery: {
+                enabled: true
+            },
+            zoom: {
+                enabled: true, // By default it's false, so don't forget to enable it
+
+                duration: 300, // duration of the effect, in milliseconds
+                easing: 'ease-in-out', // CSS transition easing function
+
+                // The "opener" function should return the element from which popup will be zoomed in
+                // and to which popup will be scaled down
+                // By defailt it looks for an image tag:
+                opener: function (openerElement) {
+                    // openerElement is the element on which popup was initialized, in this case its <a> tag
+                    // you don't need to add "opener" option if this code matches your needs, it's defailt one.
+                    return openerElement.is('img') ? openerElement : openerElement.find('img');
+                }
             }
-        }
+        });
+    };
+    // Call the functions 
+    magnifPopup();
+
+    //Background Parallax
+    $('.header-area').parallax("50%", -0.4);
+    $('.price-area').parallax("50%", -0.5);
+    $('.testimonial-area').parallax("10%", -0.2);
+
+
+    $('#accordion .panel-title a').prepend('<span></span>');
+
+
+
+
+
+
+    //Function to animate slider captions 
+    function doAnimations(elems) {
+        //Cache the animationend event in a variable
+        var animEndEv = 'webkitAnimationEnd animationend';
+
+        elems.each(function () {
+            var $this = $(this),
+                $animationType = $this.data('animation');
+            $this.addClass($animationType).one(animEndEv, function () {
+                $this.removeClass($animationType);
+            });
+        });
+    }
+
+    //Variables on page load 
+    var $myCarousel = $('.caption-slider'),
+        $firstAnimatingElems = $myCarousel.find('.item:first').find("[data-animation ^= 'animated']");
+
+    //Initialize carousel 
+    $myCarousel.carousel();
+
+    //Animate captions in first slide on page load 
+    doAnimations($firstAnimatingElems);
+
+    //Pause carousel  
+    $myCarousel.carousel('pause');
+
+
+    //Other slides to be animated on carousel slide event 
+    $myCarousel.on('slide.bs.carousel', function (e) {
+        var $animatingElems = $(e.relatedTarget).find("[data-animation ^= 'animated']");
+        doAnimations($animatingElems);
     });
 
-    /*-- Smoth-Scroll --*/
+
+
+
+
+    // Select all links with hashes
     $('.mainmenu-area a[href*="#"]')
         // Remove links that don't actually link to anything
         .not('[href="#"]')
@@ -113,44 +189,15 @@
                 }
             }
         });
-    /*--------------------
-       MAGNIFIC POPUP JS
-       ----------------------*/
-    var magnifPopup = function () {
-        $('.popup').magnificPopup({
-            type: 'iframe',
-            removalDelay: 300,
-            mainClass: 'mfp-with-zoom',
-            gallery: {
-                enabled: true
-            },
-            zoom: {
-                enabled: true, // By default it's false, so don't forget to enable it
 
-                duration: 300, // duration of the effect, in milliseconds
-                easing: 'ease-in-out', // CSS transition easing function
 
-                // The "opener" function should return the element from which popup will be zoomed in
-                // and to which popup will be scaled down
-                // By defailt it looks for an image tag:
-                opener: function (openerElement) {
-                    // openerElement is the element on which popup was initialized, in this case its <a> tag
-                    // you don't need to add "opener" option if this code matches your needs, it's defailt one.
-                    return openerElement.is('img') ? openerElement : openerElement.find('img');
-                }
-            }
-        });
-    };
-    // Call the functions
-    magnifPopup();
+
+
+
+
     /* Preloader Js
     ===================*/
     $(window).on("load", function () {
         $('.preloader').fadeOut(500);
-        /*WoW js Active
-        =================*/
-        new WOW().init({
-            mobile: false,
-        });
     });
 })(jQuery);
